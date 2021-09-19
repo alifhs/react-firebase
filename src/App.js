91888065232d647704e7b109429fce59a67b0a30
alifhs <alif.hs@outlook.com>
2021-09-19 20:53:34 +0600

    added image pop in

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Title from './comps/Title';
 import { UploadForm } from './comps/uploadform';
 import {ImageGrid} from './comps/ImageGrid';
+import {Modal} from './comps/modal';
 
 function App() {
+    const [selectedImg , setSelectImg] = useState(null);
   return (
     <div className="App">
       <Title/>
       <UploadForm></UploadForm>
-      <ImageGrid />
+      <ImageGrid  setSelectImg={setSelectImg}/>
+      {
+        selectedImg && <Modal selectedImg={selectedImg} setSelectedImg={setSelectImg}/>
+      } 
     </div>
   );
 }
--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -2,7 +2,7 @@
 import { useFirestore } from "../hooks/useFireStore";
 
 
-const ImageGrid = ()=> {
+const ImageGrid = ({setSelectImg})=> {
     const docs = useFirestore('images');
 
     console.log(docs)
@@ -11,7 +11,7 @@ const ImageGrid = ()=> {
         <div className="grid grid-cols-3 mt-8 gap-2 ">
             {docs && docs.map(doc => {
                 return <div className="" key={doc.id}>
-                    <img src={doc.url} alt="pic" />
+                    <img src={doc.url} alt="pic" onClick={()=> {setSelectImg(doc.url)}} />
                 </div>
             })}
         </div>
